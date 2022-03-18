f7c5258f5a4dccedf2183559b29933851eae7b27
Nebora <72881699+NeboraDev@users.noreply.github.com>
2022-03-18 09:42:24 -0400

    Functionality
    Doko Bot is now functional.

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+client.once("ready", () => {
+    console.log("Doko is now online!");
+})
+
+client.login(config.token);
